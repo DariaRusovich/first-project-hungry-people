8a77878765b3263a659f835725fdeafcbc84a500
DariaRusovich <rusovichdasha41@gmail.com>
2021-08-26 12:15:10 +0300

    fix-slider

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 0,
+    loop: true,
+    speed: 600,
+    navigation: {
+      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+    },
+
+    
+  });
+  var swiper = new Swiper(".myNewSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+      
+    },
+   
+  });
